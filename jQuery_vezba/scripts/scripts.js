13a073a269ca865a097a834b3e4408ebd1b45765
littlehoward <goransecujski@gmail.com>
2020-01-12 19:35:55 +0100

    sa_poslednjeg_casa

---
+++ jQuery_vezba/scripts/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    //alert('DOM učitan');
+
+    //Izmena teksta u p tagu sa jQuery uz pomoc html()
+    //$('.container p').html("Izmenjeni tekst uz pomoć jQuery-ja i html()!");
+
+    //Izmena teksta u p tagu sa jQuery uz pomoc text()
+    //$('.container p').html("Izmenjeni tekst uz pomoć jQuery-ja i text()!");
+
+    //Izmena teksta u p tagu sa JavaScript-om
+    //var x = "Ovo je novi tekst uz pomoć JavaScript-a!";
+    //document.getElementsByTagName("p")[0].innerHTML = x;
+
+    //Promena css za h3 uz pomoc jQuery-ja
+    //('.container h3').css('text-decoration', 'underline');
+    //$('.container h3').css('color', 'red');
+
+    //Promena css za h3 uz pomoc JavaScript-a
+    //var y = document.getElementsByTagName("h3")[0];
+    //y.setAttribute("style", "color: blue; font-style: italic");
+
+
+    //Prepisivanje iz input-a u prazno polje pomoću JavaScript-a
+    //function prepisi (){
+    //var z = document.forma1.tekst.value;
+    //document.getElementById("prepis").innerHTML = z;
+    //    }
+
+
+
+
+
+
+
+});
